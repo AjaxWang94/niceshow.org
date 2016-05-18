e9e8a6baf5ba84b5af745c9f5046b333e75b703e
Ajax Wang <ajaxwang94@gmail.com>
2016-05-18 14:42:22 +0800

    Modified routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var home = require('../controllers/home');
+var ticTacToe = require('../controllers/ticTacToe');
 
 /* GET home page. */
 router.get('/', home.index);
+router.get('/tic-tac-toe', ticTacToe.index);
 
 module.exports = router;
