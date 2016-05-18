fe6aa3742fb91ef04ff508834f58d05c2b5c7a8f
Ajax Wang <ajaxwang94@gmail.com>
2016-05-18 14:58:25 +0800

    Fixed some errors.

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,5 @@
 module.exports = {
   index: function(req, res, next) {
-<<<<<<< HEAD
     res.render('index', { title: 'the digital world.' });
-=======
-    res.render('index', { title: 'Express' });
->>>>>>> 372afe888c6546e8cce8bf11e81f273cf4bfe915
   }
 };
